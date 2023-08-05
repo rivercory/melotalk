4e7438ff67dc1db1e1ec261a8d964b667c6a7307
hyeonho64 <hyeonhokang10@kakao.com>
2023-08-05 09:18:07 +0900

    Version 1.2.2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Navmenu title="홈" />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {defineComponent} from "vue";
+import Navmenu from "@/components/Navmenu.vue";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+  components: {Navmenu}
+})
 </script>
