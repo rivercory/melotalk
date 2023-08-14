fe1c7c3330f59c2d4857637dbd2928579948fa9a
hyeonho64 <hyeonhokang10@kakao.com>
2023-08-14 19:16:54 +0900

    Version 2.3.8

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <Navmenu title="홈"/>
     <div class="if-desktop d-flex flex-nowrap">
       <Sidemenu/>
-      <div class="border rounded-3" style="margin-left: 1rem; margin-right: 1rem; margin-bottom: 1rem; padding: 1rem; width: 73rem;">
+      <div class="border rounded-3" style="margin-left: 1rem; margin-right: 1rem; margin-bottom: 1rem; padding: 1rem; width: 83rem;">
 
       </div>
     </div>
