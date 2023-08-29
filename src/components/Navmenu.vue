b2776a4ff9432ae1f5cd024255b2e82fbe51d129
hyeonho64 <hyeonhokang10@kakao.com>
2023-08-29 20:23:36 +0900

    Version 2.6.1

--- src/components/Navmenu.vue
+++ src/components/Navmenu.vue
@@ -12,35 +12,18 @@
       </button>
       <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel"><span style="color: #4C6EF5;">멜로</span>톡</h5>
+          <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">멜로톡</h5>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <router-link to="/" class="nav-link">홈</router-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+              <router-link to="about" class="nav-link">소개</router-link>
             </li>
           </ul>
-          <form class="d-flex mt-3" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </div>
   </nav>
@@ -72,7 +55,6 @@ export default {
 }
 
 .offcanvas-title {
-  font-family: SUIT-Regular;
-  color: #91A7FF;
+  font-family: LINESeedKR-Bd;
 }
 </style>
