a2a8c7c903f9f7a065e3ea7edffaacc72748dd31
hyeonho64 <hyeonhokang10@kakao.com>
2023-09-21 21:57:06 +0900

    Version 2.6.26

--- src/App.vue
+++ src/App.vue
@@ -28,14 +28,14 @@ body,html {
   width: 100%;
 }
 
-[data-theme="light"] {
+[color-theme="light"] {
   --background-color: #FFFFFF;
   --border-color: #ced4da;
 }
 
-[data-theme="dark"] {
+[color-theme="dark"] {
   --background-color: #212529;
-  --border-color: #FFFFFF;
+  --border-color: #adb5bd;
 }
 
 .border {
--- src/components/Navmenu.vue
+++ src/components/Navmenu.vue
@@ -17,8 +17,7 @@
               xmlns="http://www.w3.org/2000/svg"
               width="36"
               height="36"
-              fill="#000000"
-              class="bi bi-list"
+              class="navbar-toggle-icon bi bi-list"
               viewBox="0 0 16 16"
           >
             <path
@@ -28,7 +27,7 @@
           </svg>
         </button>
         <a class="navbar-brand btn" href="#">
-          <span style="color: #000000">
+          <span style="color: var(--navbar-title-color);">
             {{ title }}
           </span>
         </a>
@@ -104,7 +103,7 @@
             </li>
           </ul>
           <p class="theme-toggle rounded-3">
-            <button class="btn" @click="changeTheme('light')">
+            <button class="btn" @click="onToggleDarkMode">
               <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
@@ -114,19 +113,6 @@
               >
                 <path
                     d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"
-                />
-              </svg>
-            </button>
-            <button class="btn" @click="changeTheme('dark')">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  class="theme-toggle-icon bi bi-moon"
-                  viewBox="0 0 16 16"
-              >
-                <path
-                    d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"
                 />
               </svg>
             </button>
@@ -142,6 +128,7 @@
                 class="form-control"
                 placeholder="Search..."
                 aria-label="Search"
+                style="background-color: var(--navbar-search-area-color);"
             />
           </form>
         </div>
@@ -256,7 +243,7 @@
             </li>
           </ul>
           <p class="theme-toggle rounded-3">
-            <button class="btn" @click="changeTheme('light')">
+            <button class="btn" @click="onToggleDarkMode">
               <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
@@ -266,19 +253,6 @@
               >
                 <path
                     d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"
-                />
-              </svg>
-            </button>
-            <button class="btn" @click="changeTheme('dark')">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  class="theme-toggle-icon bi bi-moon"
-                  viewBox="0 0 16 16"
-              >
-                <path
-                    d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"
                 />
               </svg>
             </button>
@@ -294,29 +268,63 @@ export default {
   props: {
     title: String,
   },
-  methods: {
-    changeTheme(newTheme) {
-      document.documentElement.setAttribute("data-theme", newTheme);
-    },
+  mounted() {
+    if(window && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add("darkmode");
+    } else {
+      document.documentElement.classList.add("lightmode");
+    }
   },
-};
+  methods: {
+    onToggleDarkMode() {
+      if (window) {
+        console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          if(document.documentElement.classList.contains('darkmode')) {
+            document.documentElement.classList.remove("darkmode");
+            document.documentElement.classList.add("lightmode");
+          } else {
+            document.documentElement.classList.remove("lightmode");
+            document.documentElement.classList.add("darkmode");
+          }
+        } else {
+          if(document.documentElement.classList.contains('lightmode')) {
+            document.documentElement.classList.remove("lightmode");
+            document.documentElement.classList.add("darkmode");
+          } else {
+            document.documentElement.classList.remove("darkmode");
+            document.documentElement.classList.add("lightmode");
+          }
+        }
+
+      }
+    },
+  }
+}
 </script>
 
 <style>
-[data-theme="light"] {
+:root.lightmode {
   --offcanvas-icon-color: #000000;
   --offcanvas-text-color: #000000;
   --offcanvas-background-color: #ffffff;
   --offcanvas-close-button-background-color: #ffffff;
   --theme-toggle-icon-color: #000000;
+  --navbar-title-color: #000000;
+  --navbar-toggle-icon-color: #000000;
+  --navbar-search-area-color: #FFFFFF;
 }
 
-[data-theme="dark"] {
+:root.darkmode {
   --offcanvas-icon-color: #ffffff;
   --offcanvas-text-color: #ffffff;
   --offcanvas-background-color: #343a40;
   --offcanvas-close-button-background-color: #adb5bd;
   --theme-toggle-icon-color: #ffffff;
+  --navbar-title-color: #FFFFFF;
+  --navbar-toggle-icon-color: #FFFFFF;
+  --navbar-search-area-color: #adb5bd;
 }
 
 header {
@@ -356,5 +364,9 @@ header {
 
 .theme-toggle-icon {
   fill: var(--theme-toggle-icon-color);
+}
+
+.navbar-toggle-icon {
+  fill: var(--navbar-toggle-icon-color);
 }
 </style>
