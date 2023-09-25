c4063d1ef364d92b1dfe8c253ce90c0d2fa612cf
hyeonho64 <hyeonhokang10@kakao.com>
2023-09-25 19:21:58 +0900

    Version 2.6.30

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -2,6 +2,7 @@
 import {ref, onMounted} from 'vue'
 import {supabase} from '../lib/supabaseClient'
 import Navmenu from "@/components/Navmenu.vue";
+import Filter from "badwords-ko";
 
 const chat = ref([])
 
@@ -13,6 +14,7 @@ async function getChat() {
 async function inputValueChange(){
   var inputValue = document.getElementById('inputValue').value;
   const today = new Date();
+  const filter = new Filter();
   const str = inputValue;
   const strLength = str.length;
   if (strLength <= 0) {
@@ -20,7 +22,8 @@ async function inputValueChange(){
   } else {
     const { error } = await supabase
         .from('chat')
-        .insert({ created_at: today.toLocaleString(), text: inputValue })
+        .insert({ created_at: today.toLocaleString(), text: filter.clean(inputValue) })
+    location.reload()
   }
 }
 
@@ -41,8 +44,11 @@ onMounted(() => {
       <textarea class="border rounded-2" name="inputValue" id="inputValue" style="height: 10rem; width: 100%; resize: none; border: 0;"/>
       <button class="btn" @click="inputValueChange" style="float: right; margin-bottom: 1rem; background-color: #748ffc; font-weight: bold; font-family: NanumSquareNeo-Variable;">보내기</button>
       <div>
-        <div class="rounded-2" style="background-color: #e9ecef; margin-bottom: 0.5rem; padding: 1rem; clear: both;"
-             v-for="chat in chat" :key="chat.created_at">{{ chat.text }}
+        <div class="rounded-2" style="background-color: #e9ecef; margin-bottom: 0.5rem; padding: 1rem; clear: both; font-family: Pretendard-Regular;"
+             v-for="chat in chat" :key="chat.created_at">
+             <span class="date">{{ chat.created_at }}</span>
+             <br/>
+             <span class="content">{{ chat.text }}</span>
         </div>
       </div>
     </div>
@@ -51,4 +57,7 @@ onMounted(() => {
 
 
 <style>
+.content {
+  font-size: 1.2rem;
+}
 </style>
