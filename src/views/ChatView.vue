03ccd635f539c274a788c3e59e244219974a4384
hyeonho64 <hyeonhokang10@kakao.com>
2023-09-28 09:20:13 +0900

    Version 2.6.33

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,68 +1,114 @@
 <script setup>
-import {ref, onMounted} from 'vue'
-import {supabase} from '../lib/supabaseClient'
+import { ref, onMounted } from "vue";
+import { supabase } from "../lib/supabaseClient";
 import Navmenu from "@/components/Navmenu.vue";
 import Filter from "badwords-ko";
 
-const chat = ref([])
+const chat = ref([]);
 
 async function getChat() {
-  const {data} = await supabase.from('chat').select()
-  const reverse = data.reverse();
-  chat.value = reverse
+    const { data } = await supabase.from("chat").select();
+    const reverse = data.reverse();
+    chat.value = reverse;
 }
 
-async function inputValueChange(){
-  var inputValue = document.getElementById('inputValue').value;
-  const today = new Date();
-  const filter = new Filter();
-  const str = inputValue;
-  const strLength = str.length;
-  if (strLength <= 0) {
-    alert("문자를 입력해주세요!")
-  } else {
-    const { error } = await supabase
-        .from('chat')
-        .insert({ created_at: today.toLocaleString(), text: filter.clean(inputValue) })
-    location.reload()
-  }
+async function alwaysfetchChat() {
+    while  (true)  {
+        setTimeout(function  () {
+            getChat();
+        }, 1000);
+    }
 }
 
-setTimeout(function() {
-  getChat()
-}, 100);
+async function inputValueChange()  {
+    var inputValue = document.getElementById("inputValue").value;
+    const today = new Date();
+    const filter = new Filter();
+    const str = inputValue;
+    const strLength = str.length;
+    if (strLength <= 0) {
+        alert("문자를 입력해주세요!");
+    } else {
+        const {error} = await supabase
+          .from("chat")
+          .insert({
+       created_at: today.toLocaleString(),
+        text: filter.clean(inputValue),
+      });
+        getChat();
+    }
+}
 
 onMounted(() => {
-  getChat()
-})
+    getChat();
+});
 </script>
 
 <template>
-  <Navmenu title="채팅"/>
-  <div class="d-flex flex-nowrap">
-    <div class="if-desktop" style="width: 25rem; padding-left: 1rem; padding-top: 1rem; padding-bottom: 1rem;">
-      <div class="border rounded-2" style="padding: 1rem;">
-        ㄴㄷㄱ
-      </div>
+    <Navmenu title="채팅" />
+    <div class="d-flex flex-nowrap">
+        <div
+      class="if-desktop"
+      style="
+        width: 25rem;
+        padding-left: 1rem;
+        padding-top: 1rem;
+        padding-bottom: 1rem;">
+      <div class="border rounded-2" style="padding: 1rem">추가예정 레이아</div>
     </div>
-    <div style="padding: 1rem; width: 100rem;">
-      <textarea class="border rounded-2" name="inputValue" id="inputValue" style="height: 10rem; width: 100%; resize: none; border: 0;"/>
-      <button class="btn" @click="inputValueChange" style="float: right; margin-bottom: 1rem; background-color: #748ffc; font-weight: bold; font-family: NanumSquareNeo-Variable;">보내기</button>
-      <div>
-        <div class="rounded-2" style="background-color: #e9ecef; margin-bottom: 0.5rem; padding: 1rem; clear: both; font-family: Pretendard-Regular;"
-             v-for="chat in chat" :key="chat.created_at">
-             <span class="date">{{ chat.created_at }}</span>
-             <br/>
-             <span class="content">{{ chat.text }}</span>
+    <div style="padding: 1rem; width: 100rem">
+      <textarea
+        class="border rounded-2"
+        name="inputValue"
+        id="inputValue"
+        style="height: 10rem; width: 100%; resize: none; border: 0"
+      />
+      <button
+        class="btn"
+        @click="inputValueChange"
+        style="
+          float: right;
+          margin-bottom: 1rem;
+          background-color: #748ffc;
+          font-weight: bold;
+          clear: both;
+          font-family: NanumSquareNeo-Variable;
+        "
+      >
+        보내기
+      </button>
+      <div style="float: right;">
+          <button class="btn" @click="getChat">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+            </svg>
+          </button>
+      </div>
+      <div style="clear: both;">
+        <div
+          class="rounded-2"
+          style="
+            background-color: #e9ecef;
+            margin-bottom: 0.5rem;
+            padding: 1rem;
+            clear: both;
+            font-family: Pretendard-Regular;
+          "
+          v-for="chat in chat"
+          :key="chat.created_at"
+        >
+          <span class="date">{{ chat.created_at }}</span>
+          <br />
+          <span class="content">{{ chat.text }}</span>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
 <style>
 .content {
-  font-size: 1.2rem;
+    font-size: 1.2rem;
 }
 </style>
