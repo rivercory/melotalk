009927f987f734af14c48d8dfd270be6472de445
hyeonho64 <hyeonhokang10@kakao.com>
2023-10-16 20:29:50 +0900

    Version 2.7.1

--- src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://yawmhotvqamxemeypkpj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhd21ob3R2cWFteGVtZXlwa3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE4OTM1MzYsImV4cCI6MjAwNzQ2OTUzNn0.2cAZSC6oxl3JxUYMpKtAQUMRSbh8wyH9t9FFb2-GqiM')
