ff659ecae6e55b179f42b1a35029b5a51e9c23f6
hyeonho64 <hyeonhokang10@kakao.com>
2023-10-22 11:44:05 +0900

    Version 2.7.7

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('653487e30c5ad2748126');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/page/board.js
+++ src/page/board.js
@@ -7,7 +7,7 @@ const Board = () => {
     return (
         <div>
             <div style={{ float: 'right', margin: '1rem' }}>
-            <Button bg='#91a7ff'>그룹 생성하기</Button>
+            <Button bg='#748ffc' color='#FFFFFF' style={{ fontFamily: 'Cafe24Ohsquareair' }}>그룹 생성하기</Button>
             </div>
             <div style={{ clear: 'both' }}>
             <BoardCard />
