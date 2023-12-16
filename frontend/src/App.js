2c92bd5a0cf0e085ffc21ab972f44c36dfad5c62
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-16 12:07:17 +0900

    Version 2.9.9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,79 +15,78 @@ function App() {
 
     return (
         <div>
-            <div className="md:h-screen min-h-screen md:overflow-auto scrollbar-hide h-auto">
-                <div className="navbar bg-base-100 md:block hidden">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                                <HiMenu size={32}/>
-                            </div>
-                            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 suit">
-                                <li className="menu-title">멜로톡</li>
-                                <li><Link to="/">홈</Link></li>
-                                <li><Link to="/dashboard">대시보드</Link></li>
-                                <li><a>About</a></li>
-                                <li className="menu-title">We are melotalk family</li>
-                                <li><a href="https://melotalk.vercel.app">멜로톡</a></li>
-                                <li><a href="https://teenagercode.vercel.app">틴에이저코드</a></li>
-                                <li><a href="https://devlist-five.vercel.app">데브리스트</a></li>
-                                <li><a href="https://governmentplus.vercel.app">정부플러스</a></li>
-                                <li><a href="https://toolking.vercel.app">툴킹</a></li>
-                            </ul>
+            <div className="navbar fixed top-0 z-10 bg-base-100 md:block hidden">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <HiMenu size={32}/>
                         </div>
-                        <a className="btn btn-ghost text-2xl cafe24ohsquareair text-primary">
-                            <img src="./icons/melotalk_full.svg" width="26" height="26"/>
-                            멜로톡
-                        </a>
+                        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 suit">
+                            <li className="menu-title">멜로톡</li>
+                            <li><Link to="/">홈</Link></li>
+                            <li><Link to="/dashboard">대시보드</Link></li>
+                            <li><a>About</a></li>
+                            <li className="menu-title">Melotalk Universe</li>
+                            <li><a href="https://melotalk.vercel.app">멜로톡</a></li>
+                            <li><a href="https://teenagercode.vercel.app">틴에이저코드</a></li>
+                            <li><a href="https://devlist-five.vercel.app">데브리스트</a></li>
+                            <li><a href="https://governmentplus.vercel.app">정부플러스</a></li>
+                            <li><a href="https://toolking.vercel.app">툴킹</a></li>
+                        </ul>
                     </div>
-                    <div className="navbar-end">
-                        <button className="btn btn-ghost btn-circle">
-                            <HiOutlineSearch size={32}/>
-                        </button>
-                    </div>
+                    <a className="btn btn-ghost text-2xl cafe24ohsquareair text-primary">
+                        <img src="./icons/melotalk_full.svg" width="26" height="26"/>
+                        멜로톡
+                    </a>
                 </div>
-                <div className="btm-nav md:hidden">
-                    <Link to="/about">
-                        <button className="suit grid place-items-center">
-                            <IoDocumentOutline size={32}/>
-                            <span className="btm-nav-label">소개</span>
-                        </button>
-                    </Link>
-                    <Link to="/">
-                        <button className="suit grid place-items-center">
-                            <IoHomeOutline size={32}/>
-                            <span className="btm-nav-label">홈</span>
-                        </button>
-                    </Link>
-                    <Link to="/dashboard">
-                        <button className="suit grid place-items-center">
-                            <IoListOutline size={32}/>
-                            <span className="btm-nav-label">대시보드</span>
-                        </button>
-                    </Link>
-                </div>
-                <div className="p-4">
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/dashboard" element={<Dashboard/>}/>
-                    </Routes>
+                <div className="navbar-end">
+                    <button className="btn btn-ghost btn-circle">
+                        <HiOutlineSearch size={32}/>
+                    </button>
                 </div>
             </div>
-            <footer className="md:mb-0 mb-14 footer md:p-10 p-6 bg-gray-100 text-base-content">
+            <div className="btm-nav md:hidden">
+                <Link to="/about">
+                    <button className="suit grid place-items-center">
+                        <IoDocumentOutline size={32}/>
+                        <span className="btm-nav-label">소개</span>
+                    </button>
+                </Link>
+                <Link to="/">
+                    <button className="suit grid place-items-center">
+                        <IoHomeOutline size={32}/>
+                        <span className="btm-nav-label">홈</span>
+                    </button>
+                </Link>
+                <Link to="/dashboard">
+                    <button className="suit grid place-items-center">
+                        <IoListOutline size={32}/>
+                        <span className="btm-nav-label">대시보드</span>
+                    </button>
+                </Link>
+            </div>
+            <div className="md:mt-16 mt-0">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                </Routes>
+            </div>
+            <footer className="md:mb-0 mb-14 footer md:p-10 p-6 bg-gray-100 text-base-content suit">
                 <aside>
                     <img src="./icons/melotalk_transparent.svg" width="50" height="50"/>
-                    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+                    <p className="text-xl pretendard text-primary">Melotalk</p>
+                    <p>Providing reliable tech since 1992</p>
                 </aside>
                 <nav>
-                    <header className="footer-title">Services</header>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
+                    <header className="footer-title">Melotalk</header>
+                    <Link to="/" className="link link-hover">홈</Link>
+                    <Link to="/dashboard" className="link link-hover">대시보드</Link>
                     <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
                 </nav>
                 <nav>
-                    <header className="footer-title">Company</header>
-                    <a className="link link-hover">About us</a>
+                    <header className="footer-title">Melotalk Universe</header>
+                    <a href="https://melotalk.vercel.app" className="link link-hover">멜로톡</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -20,9 +20,21 @@ function Home() {
 
     return (
         <div className='App'>
+            <div className="hero min-h-screen"
+                 style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
             <h1>test 하는 중...</h1>
             <div>
-                { (typeof data.members === 'undefined') ? (
+                {(typeof data.members === 'undefined') ? (
                     <div className="flex flex-col gap-2 w-full">
                         <div className="skeleton h-4 w-full"></div>
                         <div className="skeleton h-4 w-full"></div>
