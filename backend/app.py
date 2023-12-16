3a3a3eae6ea40decfccad72e139c9be497aac8e7
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-16 19:09:40 +0900

    Version 2.9.10

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from bson.json_util import dumps
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb://localhost:27017')
+db = client['melotalk']
 
 @app.route('/data')
 def user():
@@ -11,6 +15,14 @@ def user():
         ]
     }
 
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    fruit_list = list(db.users.find({}))
+    for i in fruit_list:
+        i['_id'] = str(i['_id'])
+
+    return jsonify({'result': 'success', 'articles': fruit_list})
 
 if __name__ == '__main__':
     app.run()
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 function Home() {
     const [data, setData] = useState([{}])
 
-    useEffect(() =>
-    {
-        fetch("/data").then(
-            response => response.json()
-        ).then(
-            data => {
-                setData(data);
-                console.log(data)
-            }
-        ).catch(
-            (err) => console.log(err)
-        )
+    useEffect(() => {
+        fetch("/test").then(response => response.json()).then(data => {
+            setData(data);
+            console.log(data)
+        }).catch((err) => console.log(err))
     }, [])
 
-    return (
-        <div className='App'>
+    return (<div className='App'>
             <div className="hero min-h-screen"
                  style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
                 <div className="hero-overlay bg-opacity-60"></div>
@@ -34,17 +26,12 @@ function Home() {
             </div>
             <h1>test 하는 중...</h1>
             <div>
-                {(typeof data.members === 'undefined') ? (
-                    <div className="flex flex-col gap-2 w-full">
+                {(typeof data.articles === 'undefined') ? (<div className="flex flex-col gap-2 w-full">
                         <div className="skeleton h-4 w-full"></div>
                         <div className="skeleton h-4 w-full"></div>
-                    </div>
-                ) : (
-                    data.members.map((u) => <p>{u.name}</p>)
-                )}
+                    </div>) : (data.articles.map((u) => <p>{u.id_text.id_text}</p>))}
             </div>
-        </div>
-    )
+        </div>)
 }
 
 export default Home;
